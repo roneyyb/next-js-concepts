ff4ceeb00ba4287cfcf7f18318877ca1d4fcf137
roneyyb <bothra.rajat08@gmail.com>
2024-06-04 18:35:25 +0530

    Added not found example

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            <h2>Not Found</h2>
+            <p>Could not find requested page</p>
+        </div>
+    )
+}
+
+export default NotFound
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,11 @@
+import { notFound } from 'next/navigation'
 import React from 'react'
 
 function Review({ params: { productId, reviewId } }: { params: { productId: string, reviewId: string } }) {
-    console.log(productId, reviewId, "asdfds")
+
+    if (+reviewId > 1000) {
+        return notFound()
+    }
     return (
         <div>Review for {productId} is {reviewId}</div>
     )
