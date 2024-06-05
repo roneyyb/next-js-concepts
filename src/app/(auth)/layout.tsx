62a3bffb60a875bd0e30c603e603c5f4fb824129
roneyyb <bothra.rajat08@gmail.com>
2024-06-05 18:16:13 +0530

    Added finding active link using usePathname

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,41 @@
+"use client"
+import React from 'react'
+import Link from 'next/link'
+import { usePathname } from "next/navigation"
+import "./styles.css"
+
+const navLinks = [
+    {
+        name: "Register", href: "/register",
+
+    },
+    {
+        name: "Login", href: "/login",
+
+    },
+    {
+        name: "Forgot Password", href: "/forgot-password",
+
+    },
+
+]
+
+function Auth({ children }: { children: React.ReactNode }) {
+
+    const pathname = usePathname();
+
+
+    return (
+        <div>
+            {navLinks.map((item, index) => {
+                const isActive = pathname.startsWith(item.href);
+
+                return <Link className={isActive ? " font-bold mr-4" : " text-blue-500 mr-4"} key={index} href={item.href}>{item.name}</Link>
+
+            })}
+            {children}
+        </div>
+    )
+}
+
+export default Auth
