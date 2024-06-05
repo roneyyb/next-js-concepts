2df6ac7b87704a7a2719ab10910ef62463faffd9
roneyyb <bothra.rajat08@gmail.com>
2024-06-05 14:06:58 +0530

    Added route group layout

---
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AuthLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            AuthLayout
+            {children}
+        </div>
+    )
+}
+
+export default AuthLayout
