449216aa08a7523141a80217540baeaf3d746bf6
roneyyb <bothra.rajat08@gmail.com>
2024-06-05 17:32:02 +0530

    Dynamic routing meta data

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { Metadata } from "next"
 
-function ProductList({ params }: { params: { productId: string } }) {
+type Props = { params: { productId: string } };
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const title = await new Promise((res, rej) => {
+        setTimeout(() => { res(`iPhone ${params.productId}`) }, 1000);
+    });
+
+    return {
+        title: `Product ${title}`
+    }
+}
+
+function ProductList({ params }: Props) {
     return (
         <>
             <div>Details about product {params.productId}</div>
