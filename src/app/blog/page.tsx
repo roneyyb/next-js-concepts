357324bb159316c9f498abd83dc4d7824222ebaa
roneyyb <bothra.rajat08@gmail.com>
2024-06-05 17:52:57 +0530

    Replacing template with absolute title in Blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import {Metadata} from "next"
+import { Metadata } from "next"
 
 export const metadata: Metadata = {
-    title: "Blog"
+    title: {
+        absolute: "Blog"
+    }
 }
 
 export default function Blog() {
