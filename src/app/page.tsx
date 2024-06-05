e9842e31e5f37b55501bb14166ed1eb5ab6ecbef
roneyyb <bothra.rajat08@gmail.com>
2024-06-05 17:59:30 +0530

    Using navigation through Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Home() {
     return (
-        <div>Home</div>
+        <>
+            <div>Home</div>
+            <Link href="/blog" >Blog</Link>
+            <Link href="/products">Product</Link>
+        </>
     )
 }
 
