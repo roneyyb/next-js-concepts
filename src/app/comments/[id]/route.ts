b5965569105f391a9dd662c2c4e7fd52e2f0fb43
roneyyb <bothra.rajat08@gmail.com>
2024-07-13 20:07:13 +0530

    ADDED DELETE & PATCH ROUTE HANDLING EXAMPLES

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,4 +1,5 @@
-export { comments } from "../data"
+import { comments } from "../data";
+
 
 export async function GET(_request: Request, { params }: { params: { id: string } }) {
 
@@ -6,4 +7,28 @@ export async function GET(_request: Request, { params }: { params: { id: string 
 
     return Response.json(comment);
 
+}
+
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+    const body = request.json();
+
+    const { text } = body;
+
+    const findCommentIndex = comments.findIndex(item => +item.id === +params.id);
+
+    comments[findCommentIndex].text = text;
+
+    return Response.json(comments[findCommentIndex])
+
+}
+
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+
+    const findCommentIndex = comments.findIndex(item => +item.id === +params.id);
+
+    const deletedComment = comments[findCommentIndex];
+    comments.splice(findCommentIndex, 1);
+
+    return Response.json(deletedComment);
 }
