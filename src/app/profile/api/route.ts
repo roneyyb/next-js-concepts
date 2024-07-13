aa1052fbebad58e44bc6cba00f59d5e3b208b111
roneyyb <bothra.rajat08@gmail.com>
2024-07-13 23:28:46 +0530

    CONCEPT: HEADER IN ROUTE HANDLER

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,11 @@
-export async function GET() {
-    return new Response("Profile Data")
+import { type NextRequest } from "next/server"
+
+export async function GET(request: NextRequest) {
+    const requestHeader = new Headers(request.headers);
+    console.log(requestHeader.get("Authorization"))
+    return new Response("<h1>Profile Data</h1>", {
+        headers: {
+            "Content-Type": "text/html"
+        }
+    })
 }
