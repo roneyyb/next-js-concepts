177219b9a7c9ee351927845b3bacc0596cdff485
roneyyb <bothra.rajat08@gmail.com>
2024-07-13 23:37:32 +0530

    CONCEPT: COOKIES IN NEXTJS DEF, HOW TO SET, GET,

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,11 +1,25 @@
+import { headers, cookies } from "next/headers";
 import { type NextRequest } from "next/server"
 
 export async function GET(request: NextRequest) {
     const requestHeader = new Headers(request.headers);
+
+    const headerList = headers();
+
+    const theme = request.cookies.get("theme");
+    console.log(headerList.get("Authorization"))
+    console.log(theme)
+
+    // setting and getting cookie using cookies function
+    cookies().set("requestPerPage", "20")
+    // There are other function as well available with cookies refer documentation
+    console.log(cookies().get("requestPerPage"))
+
     console.log(requestHeader.get("Authorization"))
     return new Response("<h1>Profile Data</h1>", {
         headers: {
-            "Content-Type": "text/html"
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
         }
     })
 }
