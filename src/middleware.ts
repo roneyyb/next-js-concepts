953bea465ff714f08ebfb84f9044d09d53b4c184
roneyyb <bothra.rajat08@gmail.com>
2024-07-14 17:35:41 +0530

    MIDDLEWARE: WORKING WITH COOKIES

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,17 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+
+    const response = NextResponse.next();
+
+    const themePreference = request.cookies.get("theme");
+
+    if (!themePreference) {
+        response.cookies.set("theme", "dark");
+    }
+
+    return response;
+
     if (request.nextUrl.pathname === "/profile") {
         console.log(request.url, "url")
         return NextResponse.rewrite(new URL("/about", request.url))
