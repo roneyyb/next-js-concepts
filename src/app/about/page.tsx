78c73f3213cc62dfbc00db6f3999e5b785f6b94c
roneyyb <bothra.rajat08@gmail.com>
2024-07-16 17:11:31 +0530

    DYNAMIC RENDERING IN SSR

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,10 @@
-"use client"
-
+import { cookies } from "next/headers";
 import React from 'react'
 
 export default function ABouttt() {
+    const cookieStore = cookies();
+    const theme = cookieStore.get("theme");
+    console.log(theme);
 
     // throw new Error("Error in about")
 
