80fc980be1d4c294385ba763ff6347ce4974d930
roneyyb <bothra.rajat08@gmail.com>
2024-07-17 18:39:06 +0530

    USING CLIENT ONLY CODE

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import SimpleSlider from "@/components/carousel"
+import { clientSideFunction } from "@/utils/client-utils";
 
 // import { serverSideFunction } from "@/utils/server-utils";
 
 export default function Page() {
-    // const result = serverSideFunction();
+    const result = clientSideFunction();
     return <div>ClientRoutePage
         <SimpleSlider />
     </div>
---
+++ src/utils/client-utils.ts
@@ -0,0 +1,7 @@
+import "client-only"
+
+export const clientSideFunction = () => {
+    console.log(`use client apis like storage etc`);
+
+    return "server result"
+}
