ad6603a42e0671cfd3203eddabf71ce4432470fc
roneyyb <bothra.rajat08@gmail.com>
2024-07-20 12:35:36 +0530

    CACHING BEHAVIOUR CHANGES USING NO-STORE, DEFAULT-CACHE

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,7 +1,15 @@
 // the data is being cached by nextjs on next requests
 
+export const fetchCache = "default-cache"
+
 export default async function ProductPage() {
-    const response = await fetch("https://localhost:3002/products");
+    const response = await fetch("https://localhost:3002/products", {
+        cache: "no-store"
+    });
+
+    const response1 = await fetch("https://localhost:3002/products/1", {
+
+    });
     const users = await response.json();
     console.log("users ", users)
     return <h1>{"Product Page"}</h1>
