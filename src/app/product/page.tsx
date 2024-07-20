7a3d1b7c8ece621806ca210515187c5e78119232
roneyyb <bothra.rajat08@gmail.com>
2024-07-20 12:41:11 +0530

    OPTING OUT OF CACHE USING DYNAMIC FUNCTION

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,16 +1,26 @@
 // the data is being cached by nextjs on next requests
+import { cookies } from "next/headers";
 
-export const fetchCache = "default-cache"
 
 export default async function ProductPage() {
+
+
+    // This request will be cached
     const response = await fetch("https://localhost:3002/products", {
-        cache: "no-store"
+
     });
 
+    const users = await response.json();
+
+    const cookiesStore = cookies();
+    cookiesStore.get("theme")
+
+    // Cookies are used so data wont be cached after using cookie function
     const response1 = await fetch("https://localhost:3002/products/1", {
 
     });
-    const users = await response.json();
+    const product = await response1.json()
+
     console.log("users ", users)
     return <h1>{"Product Page"}</h1>
 }
