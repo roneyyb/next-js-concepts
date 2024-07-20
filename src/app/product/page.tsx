bed29971ab3ef84c983e52e92491d2449d6712be
roneyyb <bothra.rajat08@gmail.com>
2024-07-20 13:20:02 +0530

    CONCEPT: TIME BASED REVALIDATION

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,26 +1,28 @@
 // the data is being cached by nextjs on next requests
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 
 //Refer documentation for more details using fetchCache variable
 export default async function ProductPage() {
 
 
     // This request will be cached
-    const response = await fetch("https://localhost:3002/products", {
-
+    const response = await fetch("http://localhost:3002/products", {
+        next: {
+            revalidate: 10
+        }
     });
 
     const users = await response.json();
 
-    const cookiesStore = cookies();
-    cookiesStore.get("theme")
+    // const cookiesStore = cookies();
+    // cookiesStore.get("theme")
 
-    // Cookies are used so data wont be cached after using cookie function
-    const response1 = await fetch("https://localhost:3002/products/1", {
+    // // Cookies are used so data wont be cached after using cookie function
+    // const response1 = await fetch("https://localhost:3002/products/1", {
 
-    });
-    const product = await response1.json()
+    // });
+    // const product = await response1.json()
 
-    console.log("users ", users)
+    //  console.log("users ", users)
     return <h1>{"Product Page"}</h1>
 }
