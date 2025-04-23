cd5f06e7745ff5a0ea737ca94500a210cf380072
roneyyb <bothra.rajat08@gmail.com>
2025-04-23 12:32:19 +0530

    DOC FIX

--- src/app/complex-dashboard/@revenue/default.tsx
+++ src/app/complex-dashboard/@revenue/default.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 function DefaultComplexDashboardRevenue
     () {
     return (
-        <div>DefaultComplexDashboardRevenue
-
-        </div>
+        null
     )
 }
 
--- src/app/complex-dashboard/@users/default.tsx
+++ src/app/complex-dashboard/@users/default.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 function DefaultComplexDashboardUsers
     () {
     return (
-        <div>DefaultComplexDashboardUsers
-
-        </div>
+        null
     )
 }
 
